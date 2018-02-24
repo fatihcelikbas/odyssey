369c579e18e3050fd672eb0e275f04d1d65eb9da
EC2 Default User <ec2-user@ip-172-31-30-214.us-east-2.compute.internal>
2018-02-24 13:56:03 +0000

    Add Gravatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def avatar_url(traveler)
+        gravatar_id = Digest::MD5::hexdigest(traveler.email).downcase
+        "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
   devise_for :travelers
-  root 'application#hello'
 end
