ec78b8fdaf757aea9afe05f5fbf299e92e7d00cc
EC2 Default User <ec2-user@ip-172-31-30-214.us-east-2.compute.internal>
2018-02-25 20:07:42 +0000

    mergeAll+fixtypos

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,10 +1,10 @@
 class Tour < ApplicationRecord
   belongs_to :guide
   
-  #validates :max_persons, presence: true
-  #validates :duration, presence: true
+  validates :max_persons, presence: true
+  validates :duration, presence: true
   #validates :price, presence: true
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :city, presence: true
+  #validates :title, presence: true
+  #validates :description, presence: true
+  #validates :city, presence: true
 end
