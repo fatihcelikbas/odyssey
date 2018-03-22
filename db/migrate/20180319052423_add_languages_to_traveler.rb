fe913585f80eacb3c85c06b89bd56f9f2cd32d0d
Arthur Kasumyan <arthurkasumyan@gmail.com>
2018-03-21 18:24:38 -0700

    db fix for heroku

--- db/migrate/20180319052423_add_languages_to_traveler.rb
+++ db/migrate/20180319052423_add_languages_to_traveler.rb
@@ -1,5 +1,5 @@
 class AddLanguagesToTraveler < ActiveRecord::Migration[5.1]
   def change
-    add_column :travelers, :languages, :string, array: true, default: "{English}"
+    add_column :travelers, :languages, :string, array: true, default: '{English}'
   end
 end
