70858ad3957fa8fc98cdf51e82f72701e66ef112
Fatih <fatih.celikbas@yale.edu>
2018-04-01 19:39:29 +0000

    Test suites~

--- test/controllers/tour_controller_test.rb
+++ test/controllers/tour_controller_test.rb
@@ -1,49 +1,5 @@
 require 'test_helper'
 
 class TourControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tour_index_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get tour_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get tour_create_url
-    assert_response :success
-  end
-
-  test "should get listing" do
-    get tour_listing_url
-    assert_response :success
-  end
-
-  test "should get pricing" do
-    get tour_pricing_url
-    assert_response :success
-  end
-
-  test "should get description" do
-    get tour_description_url
-    assert_response :success
-  end
-
-  test "should get features" do
-    get tour_features_url
-    assert_response :success
-  end
-
-  test "should get location" do
-    get tour_location_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get tour_update_url
-    assert_response :success
-  end
-
+  
 end
--- test/models/traveler_test.rb
+++ test/models/traveler_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class TravelerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @traveler = Traveler.new(:one)
+  end
+  
+  test 'invalid without firstname' do
+    @traveler.firstname = nil
+    refute @traveler.valid?, 'traveler is valid without a firstname'
+    assert_not_nil @traveler.errors[:firstname], 'no validation error for firstname present'
+  end
+
+  test 'invalid without lastname' do
+    @traveler.lastname = nil
+    refute @traveler.valid?, 'traveler is valid without a lastname'
+    assert_not_nil @traveler.errors[:lastname], 'no validation error for lastname present'
+  end
+  
+  test '#tours' do
+    assert_equal 2, @traveler.tours.size
+  end
+  
+  
+  
+  
 end
