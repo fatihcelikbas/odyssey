9297bdf263ed6b9e035d97eed761c64cc7f242f9
Arthur Kasumyan <arthurkasumyan@gmail.com>
2018-05-23 09:33:29 -0700

    Set maps photo sizeto medium and make links https not http

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-    
+
     #get the gravatar or facebook profile image
     def avatar_url(traveler)
         if traveler.image
-            "http://graph.facebook.com/#{traveler.uid}/picture?type=large"
+            "https://graph.facebook.com/#{traveler.uid}/picture?type=large"
         else
             gravatar_id = Digest::MD5::hexdigest(traveler.email).downcase
             "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=150"
         end
     end
-    
+
     #define the path for signing up for payout through stripe
     def stripe_express_path
         "https://connect.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_Ce4HDJPR8lmSBIO32NejjK56UcAfbISW&scope=read_write"
